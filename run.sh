94abfd87cd61b163894f12d805add205361915da
Richard Kendall <richardjkendall@users.noreply.github.com>
2020-03-24 10:24:27 +1100

    fixing if condition for *.json files

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ cd $HOME/package
 pip3 install --upgrade --target=/output -r requirements.txt
 if [ -x install.sh ]; then ./install.sh; fi
 cp -R $HOME/package/*.py /output/.
-if [ -f $HOME/package/*.json]; then cp -R $HOME/package/*.json /output/.; fi
+if [ -f $HOME/package/*.json ]; then cp -R $HOME/package/*.json /output/.; fi
